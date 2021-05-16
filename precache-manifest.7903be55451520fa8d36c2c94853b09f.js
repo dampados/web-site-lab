c9295017fa822a42b4e111eecd9fa856ca22f906
dampados <demdevcheek@ya.ru>
2021-05-17 04:28:21 +0800

    Updates

---
+++ precache-manifest.7903be55451520fa8d36c2c94853b09f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fed68eb2eff4c19e7d76",
+    "url": "/web-site-lab/static/js/main.219c30f6.chunk.js"
+  },
+  {
+    "revision": "8291e003f86c23705357",
+    "url": "/web-site-lab/static/js/runtime~main.fe7974f7.js"
+  },
+  {
+    "revision": "7f344948fe46154a7f82",
+    "url": "/web-site-lab/static/css/2.f20bbf89.chunk.css"
+  },
+  {
+    "revision": "7f344948fe46154a7f82",
+    "url": "/web-site-lab/static/js/2.19a58edc.chunk.js"
+  },
+  {
+    "revision": "b61820fdf269a349f43683396903d0d6",
+    "url": "/web-site-lab/static/media/logo.b61820fd.svg"
+  },
+  {
+    "revision": "bec1ea2997d44a0b28dc0056a5155a7b",
+    "url": "/web-site-lab/index.html"
+  }
+];
